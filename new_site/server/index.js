e635980497927fe2b8be5487f3a39d786bf5bf79
Matthew Merritt <merrittlmatt@gmail.com>
2023-02-14 17:01:51 -0500

    Added basic api endpoint on backend

--- new_site/server/index.js
+++ new_site/server/index.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+// create get requets handling
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// listen on port
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });

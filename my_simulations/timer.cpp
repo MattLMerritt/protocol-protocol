01c229ed32211f2f13fb6396c4db95715872f8a2
OTLorz2 <chenrunbinjason@gmail.com>
2023-02-24 17:02:39 -0500

    change the timer class implementation

--- my_simulations/timer.cpp
+++ my_simulations/timer.cpp
@@ -5,13 +5,15 @@
 // ===============================================================
 
 Timer::Timer(){
-    //Timer::us_per_tick = 1000000.0 /
-
-
+    this.timer_id = 0; // default timer id
+    this.clock_rate = 1e9; // default clock rate
+    this.current_ticks = 0;
 }
 
-Timer::Timer(int64_t begin_ticks){
-    //Timer::us_per_tick = 1000000.0 /
+void Timer::setTimerId(unsigned int new_timer_id) {
+    this.timer_id = new_timer_id;
+}
 
-    
+void Timer::setClockRate(unsigned int new_clock_rate) {
+    this.clock_rate = new_clock_rate;
 }
--- my_simulations/timer.h
+++ my_simulations/timer.h
@@ -1,28 +1,16 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
-#include <stdint.h>
-
-/* TIMER
-
-currently the timer just tracks the ticks 
-(basically at every frame of a scene it will tick up and count like a frame)
-
-TO BE IMPLEMENTED:
-- have it represent seconds (fps) -> get OS to track that maybe using another API?
-
-*/
-
 class Timer {
     public:
         Timer();
-        Timer(int64_t begin_ticks);
+        void setTimerId(unsigned int new_timer_id);
+        void setClockRate(unsigned int new_clock_rate);
 
     private:
-        int64_t current_ticks;
-        //double us_per_tick;
-        //double ms_per_tick;
-
+        unsigned int timer_id;
+        unsigned int clock_rate;
+        unsigned int current_ticks;
 };
 
 #endif

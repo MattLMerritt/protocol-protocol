56f1d4291583424b8076b5118b83644337da6319
Matthew Merritt <merrittlmatt@gmail.com>
2023-02-24 23:42:41 -0500

    Added usage of addTodo prop from prop wrapper

--- react_todo_list/src/components/TodoWrapper.js
+++ react_todo_list/src/components/TodoWrapper.js
@@ -10,6 +10,7 @@ export const TodoWrapper = () => {
         setTodos([...todos, {id: uuidv4(), 
             task: todo, completed:false,
             isEditing: false}])
+            console.log(todos)
     }
 
   return (

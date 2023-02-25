90e428a5121a4564f1782bbf61f075a33449d4b9
Matthew Merritt <merrittlmatt@gmail.com>
2023-02-24 23:37:41 -0500

    created add todo prop

--- react_todo_list/src/components/TodoWrapper.js
+++ react_todo_list/src/components/TodoWrapper.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
-import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+uuidv4()
 
 export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), 
+            task: todo, completed:false,
+            isEditing: false}])
+    }
+
   return (
     <div className='TodoWrapper'>
-        <TodoForm />
+        <TodoForm addTodo={addTodo}/>
     </div>
   )
 }

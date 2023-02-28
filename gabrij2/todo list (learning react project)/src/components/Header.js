33ee564e1bdd21dcca84c1d8e4d97cbd45a406f0
Jesse Gabriel <jessegabriel517@gmail.com>
2023-02-27 22:10:46 -0500

    Added first component

---
+++ gabrij2/todo list (learning react project)/src/components/Header.js
@@ -0,0 +1,22 @@
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+  )
+}
+
+export default Header
+
+// Class based
+
+// import React from 'react'
+
+// class App extends React.Component{
+//     render() {
+//         return <h1> Hello from a class</h1>
+//     }
+// }
+
+// export default App

2e1774de00bcb296282c58c3da92e2217aabfda4
OTLorz2 <chenrunbinjason@gmail.com>
2023-02-27 21:30:41 -0500

    update js file and components dir

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
 import './App.css';
+// importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Todo List {inputText}</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
--- react-todo/src/components/Form.js
+++ react-todo/src/components/Form.js
@@ -1,20 +1,27 @@
 import React from "react";
 
 
-const Form = () => {
-    return(
+const Form = ({ setInputText }) => {
+
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+    return (
         <form>
-            <input type="text" class="todo-input" />
-            <button class="todo-button" type="submit">
-                <i class="fas fa-plus-square"></i>
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
             </button>
-            <div class="select">
-                <select name="todos" class="filter-todo">
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
                 </select>
             </div>
         </form>
     );
-}
+};
+
+export default Form;

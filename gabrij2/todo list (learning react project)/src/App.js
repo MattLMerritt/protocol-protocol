317a590f8e2e7008e4189ab2d1e0f81a65ed637a
Jesse Gabriel <jessegabriel517@gmail.com>
2023-02-28 16:04:51 -0500

    added set task feature

--- gabrij2/todo list (learning react project)/src/App.js
+++ gabrij2/todo list (learning react project)/src/App.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { AddTask } from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
     { id: 1, text: "test", day: "fadsfasd", reminder: false },
   ]);
+
+  // Add Task
+  const addTask = (task) => {
+    //Create an id
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   const deleteTask = (id) => {
@@ -25,6 +34,7 @@ const App = () => {
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (

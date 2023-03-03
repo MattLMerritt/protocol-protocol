659a9522a83815c4c14788fd035de9c768cc43b3
munipa <123514265+munipa@users.noreply.github.com>
2023-03-02 23:21:45 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;

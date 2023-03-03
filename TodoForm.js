a486ea8386958ad1230645ad9bb6c664ba906879
munipa <123514265+munipa@users.noreply.github.com>
2023-03-03 13:05:42 -0500

    Add files via upload

--- TodoForm.js
+++ TodoForm.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const TodoForm = () => {
     return (
-        <div>TodoForm</div>
+        <form className='TodoForm'>
+            <input type = "text" classname= 'todo-input' 
+            placeholder='What is the task today?'/>
+            <button type='submit ' className='todo-btn'>
+            </button>
+        </form>
     )
 }

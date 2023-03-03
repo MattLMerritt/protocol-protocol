b91185b1f5cc40d403c308d11440bf236df36b3e
munipa <123514265+munipa@users.noreply.github.com>
2023-03-03 13:26:19 -0500

    Add files via upload

--- TodoForm.js
+++ TodoForm.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+      const [value, setValue] = useState("")
+
+      const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(value)
+      }
     return (
-        <form className='TodoForm'>
-            <input type = "text" classname= 'todo-input' 
-            placeholder='What is the task today?'/>
-            <button type='submit ' className='todo-btn'>
-            </button>
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type = "text" classname= 'todo-input' placeholder='What is the task today?' onChange={(e) => setValue(e.target.setValue)}/>
+            <button type='submit ' className='todo-btn'>AddTask</button>
         </form>
     )
 }
--- TodoWrapper.js
+++ TodoWrapper.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
+import {v4 as uuidv4 } from 'uuid';
+uuidv4();
 
 export const TodoWrapper = () => {
+      const {todos, setTodos} = useState([])
+
+      const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+      }
     return (
-        <div>
-            <TodoForm />
+        <div className='TodoWrapper'>
+            <TodoForm addTodo={addTodo}/>
         </div>
     )
 }

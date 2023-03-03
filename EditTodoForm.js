ecf4290a22337aedd71aebc10b494627c27bf9d1
munipa <123514265+munipa@users.noreply.github.com>
2023-03-03 18:23:07 -0500

    Add files via upload

--- EditTodoForm.js
+++ EditTodoForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const EditTodoForm = () => {
-    return (
-        <div>EditTodoForm</div>
-    )
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();
+        // edit todo
+        editTodo(value, task.id);
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
+  )
 }
--- TodoForm.js
+++ TodoForm.js
@@ -1,19 +1,20 @@
 import React, {useState} from 'react'
 
 export const TodoForm = ({addTodo}) => {
-      const [value, setValue] = useState("")
+    const [value, setValue] = useState('');
 
-      const handleSubmit = e => {
+    const handleSubmit = (e) => {
+      // prevent default action
         e.preventDefault();
-
-        addTodo(value)
-        
-        setValue("")
-      }
-    return (
-        <form className='TodoForm' onSubmit={handleSubmit}>
-            <input type = "text" classname= 'todo-input' value={value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.setValue)}/>
-            <button type='submit ' className='todo-btn'>AddTask</button>
-        </form>
-    )
+        // add todo
+        addTodo(value);
+        // clear form after submission
+        setValue('');
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
+  )
 }

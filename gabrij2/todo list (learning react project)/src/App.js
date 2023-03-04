a9373750a355c8f63df8b9d2aab89f73b87cecd3
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-03-04 17:03:38 -0500

    Changed button to toggle form

--- gabrij2/todo list (learning react project)/src/App.js
+++ gabrij2/todo list (learning react project)/src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import { AddTask } from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     { id: 1, text: "test", day: "fadsfasd", reminder: false },
   ]);
@@ -33,8 +34,12 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+        title="Task Tracker"
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- gabrij2/todo list (learning react project)/src/components/Header.js
+++ gabrij2/todo list (learning react project)/src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 import { Button } from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   const onClick = () => {
     console.log("Click");
   };
   return (
     <header className="header">
       <h1> {title}</h1>
-      <Button color="green" text="Hello" onClick={onClick} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };

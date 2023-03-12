f9c9daf9f8b21f4cdb4a745c19be10a5ab9067dd
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-03-11 20:03:02 -0500

    Created Homepage

--- gabrij2/blog (learning fullstack project)/client/src/components/Footer.jsx
+++ gabrij2/blog (learning fullstack project)/client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;

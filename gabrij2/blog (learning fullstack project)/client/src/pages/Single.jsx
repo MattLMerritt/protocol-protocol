dc0273b15183fd08daea12ac0378976486c9080b
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-03-17 16:05:07 -0400

    Single Page design

--- gabrij2/blog (learning fullstack project)/client/src/pages/Single.jsx
+++ gabrij2/blog (learning fullstack project)/client/src/pages/Single.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. corporis!
+        </h1>
+        <p>
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea
+          dolorem ratione sit debitis deserunt repellendus numquam ab vel
+          perspiciatis
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
--- gabrij2/blog (learning fullstack project)/client/src/pages/Write.jsx
+++ gabrij2/blog (learning fullstack project)/client/src/pages/Write.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Write = () => {
-  return <div>Write</div>;
+  return (
+    <div className="write">
+      <div className="content">content</div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;

87692c996e988d988186e9c9cd8ff486e972d013
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-03-21 17:29:12 -0400

    React Hello World

---
+++ website/website-v2/protocol-protocol-website/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

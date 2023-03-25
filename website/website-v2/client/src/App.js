3935bf1f1186b30e39f2f021e5a9646a07d2288d
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-03-24 22:28:13 -0400

    Added Home-Page/Content Block Prototype

--- website/website-v2/client/src/App.js
+++ website/website-v2/client/src/App.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
 import "./styles/App.scss";
 
 const Layout = () => {
   return (
     <>
       <Sidebar />
+      <Outlet />
     </>
   );
 };
@@ -18,6 +20,12 @@ const router = createBrowserRouter([
         <Layout />
       </div>
     ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 

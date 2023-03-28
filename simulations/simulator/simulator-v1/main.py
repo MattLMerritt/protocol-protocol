8fbb6b0a0a4cc7f9c7b0a5b1fd13175e3d8984e8
Zhi Zheng <44305771+LepronlineR@users.noreply.github.com>
2023-03-28 17:04:04 -0400

    update index for main device/wires

--- simulations/simulator/simulator-v1/main.py
+++ simulations/simulator/simulator-v1/main.py
@@ -20,15 +20,15 @@ if __name__ == "__main__":
     time_steps = 10
     world_devices = {}
     world_wires = {}
-    D1 = Device(1)
-    D2 = Device(2)
-    world_devices[1] = D1
-    world_devices[2] = D2
+    D1 = Device(0)
+    D2 = Device(1)
+    world_devices[0] = D1
+    world_devices[1] = D2
 
-    w1 = Wire(1, 1, 2, world_devices, 1)
-    w2 = Wire(2, 2, 1, world_devices, 2)
-    world_wires[1] = w1
-    world_wires[2] = w2
+    w1 = Wire(0, 0, 1, world_devices, 1)
+    w2 = Wire(1, 1, 0, world_devices, 2)
+    world_wires[0] = w1
+    world_wires[1] = w2
 
     # export data
     gen_json = GenerateJson(time_steps, world_devices, world_wires)

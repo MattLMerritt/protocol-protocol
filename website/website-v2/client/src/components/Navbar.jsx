4713250b84256f4b78b2f82cdbba9e62a325c6a6
munipa <akhilmunipalli236@gmail.com>
2023-03-31 16:19:05 -0400

    full functionality of the navbar complete

--- website/website-v2/client/src/components/Navbar.jsx
+++ website/website-v2/client/src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import "../styles/Navbar.scss"
+import {useRef} from "react"
+import {FaBars, FaTimes} from "react-icons/fa"
+import protlogo from "../imgs/protlogo.png";
+import "../styles/Navbar.css"
 
 function Navbar () {
+
+
+  const navRef = useRef();
+
+  const showNavBar = () =>  {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return( 
-    
     <header>
-      <h3>Logo</h3>
-      <nav>
+       <h3>
+        <img src={protlogo} alt="" style={{ width: '100px', height: '110px' }} />
+      </h3>
+      <nav ref={navRef}>
         <a href="/#">Contact Us</a>
         <a href="/#">About</a>
         <a href="/#">Simulation</a>
+        <button className = "nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes/>
+        </button>
       </nav>
-    
+      <button className = "nav-btn" onClick={showNavBar}>
+        <FaBars/>
+      </button>
     </header>
   );
   

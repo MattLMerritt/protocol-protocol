6f1b99402643198b21c08f690a3c6c7748f05b0d
munipa <akhilmunipalli236@gmail.com>
2023-04-06 21:00:55 -0400

    made changes to font size of text on the home page

--- website/website-v2/client/src/pages/Home.jsx
+++ website/website-v2/client/src/pages/Home.jsx
@@ -5,20 +5,20 @@ const Home = () => {
   return (
     <div className="container">
       <div className="content">
-        <h1>Project Protocol Protocol</h1>
-        <h2>Description</h2>
+        <h1 className="header-size">Project Protocol Protocol</h1>
+        <h2 className="subheader-size">Description</h2>
         <blockquote>
-          <p>
+          <p className="text-block">
             A central repository for information about all types of protocols
             from electronics, to networks, and more.
           </p>
         </blockquote>
-        <p style={{ paddingTop: 20 }}>
+        <p className="text-block" style={{ paddingTop: 15 }}>
           This site aims to provide descriptions, examples, and instructions of
           how to use a variety of protocols.
         </p>
-        <h2>What is a protocol?</h2>
-        <p>
+        <h2 className="subheader-size">What is a protocol?</h2>
+        <p className="text-block">
           the system of rules and acceptable behavior used at official
           ceremonies and occasions. (
           <a href="https://dictionary.cambridge.org/us/dictionary/english/protocol">
@@ -26,25 +26,25 @@ const Home = () => {
           </a>
           )
         </p>
-        <p>
+        <p className="text-block">
           Informally; protocols are used wherever there needs to be an aggred
           upon form of communcation. Protocols are used in countless areas from
           medicine, politics, conversation, and more. This site is focused on
           all protocols related to computing, such as electrical and networking.
         </p>
-        <h2>Thanks</h2>
-        <p>
+        <h2 className="subheader-size">Thanks</h2>
+        <p className="text-block">
           This project was created out of a desire to contribute to open-source
           projects, as apart of Rensselaer Center for Open Source (
           <a href="https://rcos.io/">RCOS</a>).
         </p>
-        <p>
+        <p className="text-block">
           This project was made possible using Docsify.js, click{" "}
           <a href="https://github.com/docsifyjs/docsify/">here</a>
           to learn more.
         </p>
-        <h2>License</h2>
-        <p>MIT</p>
+        <h2 className="subheader-size">License</h2>
+        <p className="text-block">MIT</p>
       </div>
     </div>
   );

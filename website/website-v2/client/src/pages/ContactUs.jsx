9eb600b1321a08944f95734dc43f0834f1be9fdb
munipa <akhilmunipalli236@gmail.com>
2023-04-17 22:37:55 -0400

    created a contact page, still needs fixing

--- website/website-v2/client/src/pages/ContactUs.jsx
+++ website/website-v2/client/src/pages/ContactUs.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../styles/Content.scss";
-import linkedinlogo from "../imgs/linkedin-logo-linkedin-icon-transparent-free-png.webp";
-
+import instagramlogo from "../imgs/Instagram_icon.webp";
+import gmaillogo from "../imgs/gmail_logo.png";
+import facebooklogo from "../imgs/facebook_logo.png";
+import linkendinlogo from "../imgs/linkedin-logo-linkedin-icon-transparent-free-png.webp";
 
 const ContactUs = () => {
   return (
@@ -10,28 +11,37 @@ const ContactUs = () => {
       <div className="contact-us">
         <div>Contact Us</div>{" "}
       </div>
-    <div className="content-container">
-      <div className="text-container">
-            <p className="contact-info">
-              Member 1 contact info:
-            </p>
-        </div>
+      <div className="content-container">
         <div className="image-container">
-          <Link
-            className="link"
-            to="https://www.linkedin.com/in/akhil-munipalli-11a5a9255/"
-          >
-          <img
-            classname="link-img"
-            src={linkedinlogo}
-            alt=""
-            style={{ width: "50px", height: "70px" }}
-          />
-          </Link>
+          <div>
+            <img
+              className="link-img"
+              src={instagramlogo}
+              alt=""
+              style={{ width: "275px", height: "150px" }}
+            />
+          </div>
+          <div>
+            <img
+              className="link-img"
+              src={facebooklogo}
+              alt=""
+              style={{ width: "275px", height: "150px" }}
+            />
+          </div>
+          <div>
+            <img
+              className="link-img"
+              src={gmaillogo}
+              alt=""
+              style={{ width: "300px", height: "150px" }}
+            />
+          </div>
         </div>
+        
       
+      </div>
     </div>
-  </div>
   );
 };
 
